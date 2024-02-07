83b169efdeb4b32ec8ceeb86ee4afc2fd3f8e15e
WillOtheGrimm <ayman010905@gmail.com>
2024-02-07 14:29:54 -0500

    Task 1 done, animation and damage calculation done.

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    Animator animator;
+    bool clickingOnself = false;
+    public float health;
+    public float maxHealth = 5;
+    bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>(); 
+       animator = GetComponent<Animator>();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+     movement = destination - (Vector2)transform.position;
+
+        if (isDead) return;
+
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+
+
+
+    }
+
+
+
+
+    void Update()
+    {
+        if (isDead) return;
+        if (Input.GetMouseButtonDown(0) && !clickingOnself)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+        clickingOnself |= true;
+        animator.SetTrigger("TakeDamage");
+        TakeDamage(1);
+    }
+
+    private void OnMouseUp()
+    {
+        clickingOnself &= false;
+    }
+
+
+
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0 , maxHealth);
+
+
+        if(health <= 0)
+        {
+            animator.SetTrigger("Death");
+            isDead = true;
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+            isDead = false;
+        }
+
+
+
+    }
+
+
+
+
+}
+
