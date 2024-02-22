0b1ea73a5255a0a2292cc53754ac53000a354707
WILLOPC\Ayman010905 <ayman010905@gmail.com>
2024-02-22 00:55:54 -0500

    Finished task 4b: goalkepper moves using movetoward now

--- Assets/Week 7/Script/GoalkeeperController.cs
+++ Assets/Week 7/Script/GoalkeeperController.cs
@@ -11,6 +11,7 @@ public class GoalkeeperController : MonoBehaviour
     public GameObject goalKeeper;
     Vector2 direction;
     public float maxDistance = 3;
+    public float movingSpeed = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +28,13 @@ public class GoalkeeperController : MonoBehaviour
 
         if (Vector3.Distance(Controller.CurrentSelection.transform.position, transform.position) < maxDistance * 2)
         {
-            goalKeeper.transform.position = transform.position + (Vector3)(Vector3.Distance(Controller.CurrentSelection.transform.position, transform.position) /2 * direction);
+            goalKeeper.transform.position =  Vector3.MoveTowards(goalKeeper.transform.position ,(transform.position + (Vector3)(Vector3.Distance(Controller.CurrentSelection.transform.position, transform.position) /2 * direction)), movingSpeed * Time.deltaTime);
         }
         else
         {
-            goalKeeper.transform.position = transform.position + (Vector3)(maxDistance * direction);
+            goalKeeper.transform.position = Vector3.MoveTowards( goalKeeper.transform.position , (transform.position + (Vector3)(maxDistance * direction)), movingSpeed * Time.deltaTime);
+                
+                
         }
 
     }
